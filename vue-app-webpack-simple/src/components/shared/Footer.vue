a0f0481d6625a4b3afe035932e1416df8543122d
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-15 11:45:00 -0500

    updated header/ footer components

--- vue-app-webpack-simple/src/components/shared/Footer.vue
+++ vue-app-webpack-simple/src/components/shared/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>All Servers are managed here</p>
+<div class="row">
+  <div class="col-xs-12">
+    <footer>
+      <p>All Servers are managed here</p>
+    </footer>
+  </div>
+</div>
 </template>
 
 <script>
