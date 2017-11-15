193c5e7aa1186d0b4bceb571bba15fb86693955e
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-15 15:07:34 -0500

    added some more advanced functionality (continued)

--- advanced-app/src/components/Quote.vue
+++ advanced-app/src/components/Quote.vue
@@ -2,9 +2,12 @@
 <div>
   <div class="title">
     <slot name="title"></slot>
+    <span style="color: #ccc">
+      <slot name="subtitle">The Default Subtitle</slot>
+    </span>
   </div>
   <div>
-    <slot name="content"></slot>
+    <slot></slot>
   </div>
 </div>
 </template>
