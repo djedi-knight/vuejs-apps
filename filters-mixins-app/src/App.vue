1a1feb4120d41b22fdb333b375668d6738f07a00
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-16 15:05:03 -0500

    created a local filter

--- filters-mixins-app/src/App.vue
+++ filters-mixins-app/src/App.vue
@@ -2,7 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>    
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase }}</p>   
       </div>
     </div>
   </div>
@@ -10,7 +11,16 @@
 
 <script>
   export default {
-
+    data() {
+      return {
+        text: 'Hello there!'
+      }
+    },
+    filters: {
+      toUppercase(value) {
+        return value.toUpperCase()
+      }
+    }
   }
 </script>
 
