c7120b5581bbe814068a813f9ea612adafed719e
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-16 15:11:15 -0500

    created global filter

--- filters-mixins-app/src/App.vue
+++ filters-mixins-app/src/App.vue
@@ -3,7 +3,8 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase }}</p>   
+        <p>{{ text1 | toUppercase }}</p>
+        <p>{{ text2 | toUppercase | to-lowercase }}</p>
       </div>
     </div>
   </div>
@@ -13,7 +14,8 @@
   export default {
     data() {
       return {
-        text: 'Hello there!'
+        text1: 'text for upper case filter',
+        text2: 'TEXT FOR LOWER CASE FILTER'
       }
     },
     filters: {
