f504b83b68f3460c0f01e6825c4284b5be2f7d8e
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-17 15:54:06 -0500

    setup form

--- http-app/src/App.vue
+++ http-app/src/App.vue
@@ -3,6 +3,15 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>HTTP</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label>E-Mail</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary">Submit</button>
       </div>
     </div>
   </div>
@@ -10,6 +19,14 @@
 
 <script>
 export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        email: ''
+      }
+    }
+  }
 }
 </script>
 
