27f016ca0513b8bb0762484aac8fdfdc5f4aa7bd
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-17 14:14:43 -0500

    setup js animation code

--- animations-app/src/App.vue
+++ animations-app/src/App.vue
@@ -28,6 +28,21 @@
         <transition :name="alertAnimation">
           <div class="alert alert-info" v-if="show">This is a dynamic css transition</div>
         </transition>
+        <br>
+        <hr>
+        <button class="btn btn-primary" @click="load = !load">Load/ Remove Element</button>
+        <br><br>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled">
+          <div style="width: 100px; height: 100px; background-color: lightgreen;" v-if="load"></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -38,8 +53,37 @@
     data() {
       return {
         show: true,
+        load: true,
         alertAnimation: 'fade'
       }
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter')
+      },
+      enter(el, done) {
+        console.log('enter')
+        done()
+      },
+      afterEnter(el) {
+        console.log('afterEnter')
+      },
+      enterCancelled(el) {
+        console.log('enterCancelled')
+      },
+      beforeLeave(el) {
+        console.log('beforeLeave')
+      },
+      leave(el, done) {
+        console.log('leave')
+        done()
+      },
+      afterLeave(el) {
+        console.log('afterLeave')
+      },
+      leaveCancelled(el) {
+        console.log('leaveCancelled')
+      },
     }
   }
 </script>
