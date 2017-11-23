152c676bf4f015addf232a898e4e02321f4e35e0
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 18:51:17 -0500

    creater Header component with route links

---
+++ routing-app/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <ul class="nav nav-pills">
+    <li role="presentation">
+      <router-link to="/">Home</router-link>
+    </li>
+    <li role="presentation">
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>
+</div>
+</template>
