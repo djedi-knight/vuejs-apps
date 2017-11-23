a84dcdd43c73ad4926ea6b2a362a72aeed6e0ec9
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 12:13:42 -0500

    implemented resource

--- http-app/src/App.vue
+++ http-app/src/App.vue
@@ -31,18 +31,20 @@ export default {
         username: '',
         email: ''
       },
-      users: []
+      users: [],
+      resource: {}
     }
   },
   methods: {
     submit() {
-      this.$http
-        .post('data.json', this.user)
-        .then(response => {
-          console.log(response)
-        }, error => {
-          console.log(error)
-        })
+      // this.$http
+      //   .post('data.json', this.user)
+      //   .then(response => {
+      //     console.log(response)
+      //   }, error => {
+      //     console.log(error)
+      //   })
+      this.resource.save({}, this.user)
     },
     fetchData() {
       this.$http
@@ -56,6 +58,10 @@ export default {
           this.users = resultsArray
         })
     }
+  },
+  created() {
+    // setup VueResource
+    this.resource = this.$resource('data.json')
   }
 }
 </script>
