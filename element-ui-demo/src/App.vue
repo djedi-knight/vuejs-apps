fc9c55f4baffc1bdc9a27a7773ff6cc00bbd7299
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 11:07:55 -0500

    created element-ui-demo app

---
+++ element-ui-demo/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+    <hr>
+
+    <el-button :plain="true" @click="open">message</el-button>
+    <el-button :plain="true" @click="open2">success</el-button>
+    <el-button :plain="true" @click="open3">warning</el-button>
+    <el-button :plain="true" @click="open4">error</el-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  // components: {
+  //   HelloWorld
+  // }
+  methods: {
+    open() {
+      this.$message('This is a message.');
+    },
+    open2() {
+      this.$message({
+        message: 'Congrats, this is a success message.',
+        type: 'success'
+      });
+    },
+    open3() {
+      this.$message({
+        message: 'Warning, this is a warning message.',
+        type: 'warning'
+      });
+    },
+    open4() {
+      this.$message.error('Oops, this is a error message.');
+    }
+  }
+}
+</script>
+
+<style>
+.el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
