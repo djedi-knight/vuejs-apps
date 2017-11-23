e008a4d2f5b93e55aa6067b08b0e422997eb554e
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 18:32:20 -0500

    created/ implemented initial routes

--- routing-app/src/main.js
+++ routing-app/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from './routes'
+
+// install VueRouter
+Vue.use(VueRouter)
+
+// create VueRouter
+const router = new VueRouter({routes}) // ES6: equivalent to {routes: routes}
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ routing-app/src/routes.js
@@ -0,0 +1,13 @@
+import Home from './components/Home.vue'
+import User from './components/User/User.vue'
+
+export const routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: '/user',
+    component: User
+  }
+]
