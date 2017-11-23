c0871103627fb15fe46bb364b7a45bf3188b30e9
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 12:03:02 -0500

    setup an example interceptor

--- http-app/src/main.js
+++ http-app/src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 
-// Configure VueResource
+// Setup VueResource
 Vue.use(VueResource)
+
+// Configure VueResource root url
 Vue.http.options.root = 'https://vuejs-http-466e8.firebaseio.com'
 
+// Configure VueResource interceptors
+Vue.http.interceptors.push((request, next) => {
+  console.log('request', request)
+  next(response => {
+    console.log('response', response)
+  })
+})
 
 new Vue({
   el: '#app',
