28d9513dfe773c573d07513d36e499c8ebe772ab
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 20:26:01 -0500

    added router links in UserStart component

--- routing-app/src/components/user/UserDetail.vue
+++ routing-app/src/components/user/UserDetail.vue
@@ -1,3 +1,12 @@
 <template>
-<h3>Some User Details</h3>
+  <div>
+    <h3>Some User Details</h3>
+    <p>User ID: {{ userId }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['userId']
+}
+</script>
