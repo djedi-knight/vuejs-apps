e45d4da9a36e18e1570ad20108d4295f27cb437b
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-24 15:16:17 -0500

    defined router scrolling behaviour

--- routing-app/src/main.js
+++ routing-app/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import { routes } from './routes'
+import {
+  routes
+} from './routes'
 
 // install VueRouter
 Vue.use(VueRouter)
@@ -9,7 +11,21 @@ Vue.use(VueRouter)
 // create VueRouter
 const router = new VueRouter({
   routes, // ES6: equivalent to {routes: routes}
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    // scroll back to saved position if present
+    if (savedPosition) {
+      return savedPosition
+    }
+    // if hash is present, scroll to it
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    // default: scroll to the top of the page
+    return {x: 0, y: 0}
+  }
 })
 
 new Vue({
