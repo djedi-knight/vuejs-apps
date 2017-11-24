789f29d94eaf9e4485f22fa8fe742f3e098289ba
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-24 15:06:55 -0500

    passed hash fragment route to UserEdit component

--- routing-app/src/components/user/UserDetail.vue
+++ routing-app/src/components/user/UserDetail.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <h3>Some User Details</h3>
-    <p>User ID: {{ userId }}</p>
-    <router-link :to="{ name: 'userEdit', query: { locale: 'en', q: 100 }}" tag="button" class="btn btn-primary">
-      Edit User
-    </router-link>
-    <hr>
-  </div>
+<div>
+  <h3>Some User Details</h3>
+  <p>User ID: {{ userId }}</p>
+  <router-link :to="editLink" tag="button" class="btn btn-primary">
+    Edit User
+  </router-link>
+  <router-link :to="editAnchorLink" tag="button" class="btn btn-warning">
+    Edit User (Jump to Extra Data Section)
+  </router-link>
+  <hr>
+</div>
 </template>
 
 <script>
 export default {
-  props: ['userId']
+  props: ['userId'],
+  data() {
+    return {
+      editLink: {
+        name: 'userEdit',
+        query: {
+          locale: 'en',
+          q: 100
+        }
+      },
+      editAnchorLink: {
+        name: 'userEdit',
+        hash: '#data'
+      }
+    }
+  }
 }
 </script>
--- routing-app/src/components/user/UserEdit.vue
+++ routing-app/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <div style="height: 700px"></div>
+    <p id="data">Some Extra Data</p>
     <hr>
   </div>
 </template>
