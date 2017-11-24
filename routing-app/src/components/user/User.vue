e8b6f8ed3e3071a636eda6358b347a82204733e2
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 19:29:02 -0500

    added route parameter example

--- routing-app/src/components/user/User.vue
+++ routing-app/src/components/user/User.vue
@@ -2,15 +2,21 @@
 <div>
   <h1>The User Page</h1>
   <hr>
+  <p>Loaded ID: {{ userId }}</p>
   <button class="btn btn-primary" @click="navigateToHome">Go Home</button>
 </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userId: this.$route.params.id
+    }
+  },
   methods: {
     navigateToHome() {
-      this.$router.push({path: '/'})
+      this.$router.push('/')
     }
   }
 }
