47a4cbf7e714197fb37ecb15473e6be8e2e24667
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-23 20:56:40 -0500

    implemented query parameters

--- routing-app/src/components/user/UserDetail.vue
+++ routing-app/src/components/user/UserDetail.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Some User Details</h3>
     <p>User ID: {{ userId }}</p>
-    <router-link :to="{ name: 'userEdit' }" tag="button" class="btn btn-primary">
+    <router-link :to="{ name: 'userEdit', query: { locale: 'en', q: 100 }}" tag="button" class="btn btn-primary">
       Edit User
     </router-link>
   </div>
