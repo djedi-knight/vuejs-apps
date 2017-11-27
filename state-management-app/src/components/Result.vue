b35527bf69246abd6ccd0d69f2bdbfddf50940d0
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-26 22:45:06 -0500

    implemented mapGetters vuex helper method

--- state-management-app/src/components/Result.vue
+++ state-management-app/src/components/Result.vue
@@ -1,11 +1,14 @@
 <template>
 <div>
+  <p>{{ stringCounter }}</p>
   <p>Counter is: {{ counter }}</p>
   <p>Double counter is: {{ doubleCounter }}</p>
 </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
     counter() {
@@ -13,7 +16,8 @@ export default {
     },
     doubleCounter() {
       return this.$store.getters.doubleCounter
-    }
+    },
+    ...mapGetters(['stringCounter'])
   }
 }
 </script>
