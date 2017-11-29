53ba080ef2cfe6af4fb769ae26df7983b6aa347c
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-29 01:11:13 -0500

    implemented props/ data on Stock component

--- stock-trader-app/src/components/stocks/Stock.vue
+++ stock-trader-app/src/components/stocks/Stock.vue
@@ -3,13 +3,13 @@
   <div class="panel panel-success">
     <div class="panel-heading">
       <h3 class="panel-title">
-        STOCK NAME
-        <small>(Price: PRICE)</small>
+        {{ stock.name }}
+        <small>(Price: {{ stock.price }})</small>
       </h3>
     </div>
     <div class="panel-body">
       <div class="pull-left">
-        <input type="number" class="form-control" placeholder="Quantity">
+        <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
       </div>
       <div class="pull-right">
         <button class="btn btn-success">Buy</button>
@@ -18,3 +18,14 @@
   </div>
 </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  }
+}
+</script>
--- stock-trader-app/src/components/stocks/Stocks.vue
+++ stock-trader-app/src/components/stocks/Stocks.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
-  <app-stock v-for="stock in stocks"></app-stock>
+  <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
 </div>
 </template>
 
 <script>
 import Stock from './Stock'
+
 export default {
   data() {
     return {
