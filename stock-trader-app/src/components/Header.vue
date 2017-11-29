848d77a5089cd626237b30c9ea985b0b7f38de7f
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-29 16:53:15 -0500

    implemented saveData() method

--- stock-trader-app/src/components/Header.vue
+++ stock-trader-app/src/components/Header.vue
@@ -19,13 +19,13 @@
 
       <ul class="nav navbar-nav navbar-right">
         <li><a @click="endDay">End Day</a></li>
-        <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen"">
+        <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-hasexpanded="false">
             Save & Load <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="#">Save Data</a></li>
-            <li><a href="#">Load Data</a></li>
+            <li><a href="#" @click="saveData">Save Data</a></li>
+            <li><a href="#" @click="loadData">Load Data</a></li>
           </ul>
         </li>
       </ul>
@@ -56,6 +56,20 @@ export default {
     ...mapActions(['randomizeStocks']),
     endDay() {
       this.randomizeStocks()
+    },
+    saveData() {
+      // create data object from store data
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      }
+
+      // put data onto the server
+      this.$http.put('data.json', data)
+    },
+    loadData() {
+
     }
   }
 }
