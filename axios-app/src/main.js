7c542bf2cef00bd3f512a0a55f42f5b911d5c7d5
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-11-30 13:05:43 -0500

    added request/ response interceptors

--- axios-app/src/main.js
+++ axios-app/src/main.js
@@ -10,6 +10,16 @@ axios.defaults.baseURL = 'https://axios-app.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'sample_auth_header'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+axios.interceptors.request.use(request => {
+  console.log('request (interceptor)', request)
+  return request
+})
+
+axios.interceptors.response.use(response => {
+  console.log('response (interceptor)', response)
+  return response
+})
+
 new Vue({
   el: '#app',
   router,
